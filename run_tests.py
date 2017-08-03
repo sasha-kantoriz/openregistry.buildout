41c1be097bbf78073bb543060fec6a72c5dc5b44
Andrew Leitsius <leits@quintagroup.com>
2017-08-03 15:59:22 +0300

    Update run_tests

--- run_tests.py
+++ run_tests.py
@@ -1,14 +1,22 @@
 import sys
 import nose
-import pkg_resources
+from pkg_resources import iter_entry_points
 
-suites = [ep.load() for ep in
-          pkg_resources.iter_entry_points(group='openregistry.tests')]
+cover_packages = list()
+all_tests = list()
 
-all_tests = []
-for suite in suites:
+for entry_point in iter_entry_points(group='openregistry.tests'):
+    cover_packages.append("openregistry.{}".format(entry_point.name))
+    suite = entry_point.load()
     for tests in suite():
         all_tests.append(tests)
 
+nose_env = {
+    "NOSE_WITH_COVERAGE": 1,
+    "NOSE_COVER_PACKAGE": cover_packages,
+    "NOSE_COVER_ERASE": 1,
+    "NOSE_COVER_HTML": 1
+}
+
 if __name__ == '__main__':
-    sys.exit(nose.run_exit(suite=all_tests))
+    sys.exit(nose.run_exit(suite=all_tests, env=nose_env))
