04bc913cb89c3a914a9ce721fbbd95edb5863ec5
Scandie <grizly.vl@gmail.com>
2017-10-05 13:53:34 +0300

    Add support of pytest tests to run_test.py script

--- run_tests.py
+++ run_tests.py
@@ -4,12 +4,16 @@ from pkg_resources import iter_entry_points
 
 cover_packages = list()
 all_tests = list()
+pytests = list()
 
 for entry_point in iter_entry_points(group='openregistry.tests'):
     cover_packages.append("openregistry.{}".format(entry_point.name))
     suite = entry_point.load()
     for tests in suite():
         all_tests.append(tests)
+
+for entry_point in iter_entry_points(group='openregistry.pytests'):
+    pytests.append(entry_point.load())
 
 nose_env = {
     "NOSE_WITH_COVERAGE": 1,
@@ -19,4 +23,5 @@ nose_env = {
 }
 
 if __name__ == '__main__':
-    sys.exit(nose.run_exit(suite=all_tests, env=nose_env))
+    sys.exit([suite() for suite in pytests],
+             nose.run_exit(suite=all_tests, env=nose_env))
